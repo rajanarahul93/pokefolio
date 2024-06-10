1f99386ae7fe14f7ca133bb4039c058c71e4c043
rajanarahul93 <rajanarahul93@gmail.com>
2024-06-10 21:43:25 +0530

    Changed

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,19 +1,18 @@
+import React from 'react';
 import useDebounce from '../../hooks/UseDebounce';
 
-
-function Search({updateSearchTerm}){
-    const debounceCallBack = useDebounce((e)=> updateSearchTerm(e.target.value))
-    return(
-        <div className="search-wrapper w-screen h-16  flex items-center justify-center"> 
-            <input 
-                // id ="pokemon-name-search w-44"
+function Search({ updateSearchTerm }) {
+    const debounceCallBack = useDebounce((e) => updateSearchTerm(e.target.value));
+    return (
+        <div className="search-wrapper w-screen h-16 flex items-center justify-center">
+            <input
                 className="w-3/5 h-4/5 mt-4 rounded-md border-solid pl-4 border-slate-500 border-2"
-                type="text" 
-                placeholder="Pokemon name...." 
+                type="text"
+                placeholder="Pokemon name...."
                 onChange={debounceCallBack}
             />
         </div>
-    )
+    );
 }
 
 export default Search;
